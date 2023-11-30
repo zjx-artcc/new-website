57a68b30c800a04070dd663cd71cb1f69cbfa4ac
svalencia014 <sv.samuel.valencia014@gmail.com>
2023-11-30 18:57:53 -0500

    start work

---
+++ src/components/Navbar.svelte
@@ -0,0 +1,2 @@
+<img class="h-10" src="/ZJX-Light-Logo.png">
+<a href='/' class="text-xl text-stone-200" id="index">Pilot/VA Assistance</a>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+  import '../app.css';
+  import Navbar from '../components/Navbar.svelte';
+</script>
+<header class="topnav bg-slate-900 justify-between h-16 flex my-0 mx-auto" id="myTopnav">
+  <Navbar />
+</header>
