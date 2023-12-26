869424e88273bf3bddbffb1793851b1cce53088c
svalencia014 <sv.samuel.valencia014@gmail.com>
2023-12-26 11:20:26 -0500

    fixy

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -1,10 +1,12 @@
-<div id="logo-box" class="flex-[0_0_auto]">
-  <div id="logo-item" class="inline-block">
-    <img class="h-10" src="/ZJX-Light-Logo.png" alt="ZJX Logo">
-  </div>
+<div style="display: inline-block; height: 34px; line-height: 34px; vertical-align: middle;">
+  <a class="link-effect font-w700" href="/">
+       <span class="font-size-xl text-dual-primary-dark">Jacksonville</span>
+      <span class="font-size-xl text-primary"> ARTCC</span>
+  </a>
 </div>
-<a href='/' class="text-xl text-stone-200" id="index">Pilot/VA Assistance</a>
-<a href='/' class="text-xl text-stone-200" id="index">Calendar/Events</a>
-<a href='/' class="text-xl text-stone-200" id="index">Facilities</a>
-<a href='/' class="text-xl text-stone-200" id="index">Membership</a>
-<a href='/' class="text-xl text-stone-200" id="index">Profile/Information</a>
+<a href='/' class="text-xl text-stone-200" id="index">Home</a>
+<a href='/' class="text-xl text-stone-200" id="index">Pilots</a>
+<a href='/' class="text-xl text-stone-200" id="index">Controllers</a>
+<a href='/' class="text-xl text-stone-200" id="index">Social Media</a>
+<a href='/' class="text-xl text-stone-200" id="index">Events</a>
+<a href='/' class="text-xl text-stone-200" id="index">Login</a>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,4 +6,6 @@
   <div class="justify-between flex flex-row max-w-6xl items-center my-0 mx-auto">
     <Navbar />
   </div>
+  <div style="background-position: 0% 50%; background-size: cover; background-image: url('/KJAXNIGHT.png'); left: 0; top: 0; height: 560px; ">
+  </div>
 </header>
