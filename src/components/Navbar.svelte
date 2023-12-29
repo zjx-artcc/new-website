4a5ed564286d13225193f57f112e3c1989e6cdfe
svalencia014 <sv.samuel.valencia014@gmail.com>
2023-12-29 10:03:14 -0500

    update navbar

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -1,12 +1,15 @@
+<script>
+  import Icon from '@iconify/svelte';
+</script>
+
 <div style="display: inline-block; height: 34px; line-height: 34px; vertical-align: middle;">
-  <a class="link-effect font-w700" href="/">
-       <span class="font-size-xl text-dual-primary-dark">Jacksonville</span>
-      <span class="font-size-xl text-primary"> ARTCC</span>
+  <a class="link-effect font-bold" href="/">
+      <span class="text-xl text-stone-50">Jacksonville</span>
+      <span class="text-xl text-sky-500"> ARTCC</span>
   </a>
 </div>
-<a href='/' class="text-xl text-stone-200" id="index">Home</a>
-<a href='/' class="text-xl text-stone-200" id="index">Pilots</a>
-<a href='/' class="text-xl text-stone-200" id="index">Controllers</a>
-<a href='/' class="text-xl text-stone-200" id="index">Social Media</a>
-<a href='/' class="text-xl text-stone-200" id="index">Events</a>
-<a href='/' class="text-xl text-stone-200" id="index">Login</a>
+<a href='/' class="text-lg text-stone-200 inline-flex" id="index"><Icon icon="mdi:airplane" class="pt-1.5" style="width: 30px; height: 25px;"/> Pilots</a>
+<a href='/' class="text-lg text-stone-200 inline-flex" id="index"><Icon icon="ri:radar-fill" class="pt-1.5" style="width: 30px; height: 25px" /> Controllers</a>
+<a href='/' class="text-lg text-stone-200 inline-flex" id="index"><Icon icon="ic:baseline-people" class="pt-1.5" style="width: 30px; height: 25px" /> Membership</a>
+<a href='/' class="text-lg text-stone-200 inline-flex" id="index"><Icon icon="ion:calendar" class="pt-1.5" style="width: 30px; height: 25px" /> Events</a>
+<a href='/' class="text-lg text-stone-200 inline-flex" id="index"><Icon icon="ic:baseline-log-in" class="pt-1.5" style="width: 30px; height: 25px" /> Login</a>
