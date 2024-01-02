e266cbaab8baa0bf3024a8080530a78934f5c9cf
Deonte Palmer <ferfrink3577@gmail.com>
2024-01-01 20:22:54 -0600

    reformat: code readibility

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -1,27 +1,41 @@
 <script lang="ts">
-  import Icon from '@iconify/svelte';
+	import Icon from '@iconify/svelte';
 
-  function showDropdown(page: string) {
-    let parent = document.getElementById(page);
-    if (parent == null) {
-      return;
-    }
-    if (parent.classList.contains("responsive")){
-      parent.classList.remove("responsive");
-    } else {
-      parent.classList.add('responsive');
-    }
-  }
+	function showDropdown(page: string) {
+		let parent = document.getElementById(page);
+		if (parent == null) {
+			return;
+		}
+		if (parent.classList.contains('responsive')) {
+			parent.classList.remove('responsive');
+		} else {
+			parent.classList.add('responsive');
+		}
+	}
 </script>
 
 <div style="display: inline-block; height: 34px; line-height: 34px; vertical-align: middle;">
-  <a class="link-effect font-bold" href="/">
-      <span class="text-xl text-stone-50">Jacksonville</span>
-      <span class="text-xl text-sky-500"> ARTCC</span>
-  </a>
+	<a class="link-effect font-bold" href="/">
+		<span class="text-xl text-stone-50">Jacksonville</span>
+		<span class="text-xl text-sky-500"> ARTCC</span>
+	</a>
 </div>
-<a href='/' class="text-lg text-stone-200 inline-flex" id="pilots" on:click={() => showDropdown("pilots")}><Icon icon="mdi:airplane" class="pt-1.5" style="width: 30px; height: 25px;"/> Pilots</a>
-<a href='/' class="text-lg text-stone-200 inline-flex" id="index"><Icon icon="ri:radar-fill" class="pt-1.5" style="width: 30px; height: 25px" /> Controllers</a>
-<a href='/' class="text-lg text-stone-200 inline-flex" id="index"><Icon icon="ic:baseline-people" class="pt-1.5" style="width: 30px; height: 25px" /> Membership</a>
-<a href='/' class="text-lg text-stone-200 inline-flex" id="index"><Icon icon="ion:calendar" class="pt-1.5" style="width: 30px; height: 25px" /> Events</a>
-<a href='/' class="text-lg text-stone-200 inline-flex" id="index"><Icon icon="ic:baseline-log-in" class="pt-1.5" style="width: 30px; height: 25px" /> Login</a>
+<a
+	href="/"
+	class="text-lg text-stone-200 inline-flex"
+	id="pilots"
+	on:click={() => showDropdown('pilots')}
+	><Icon icon="mdi:airplane" class="pt-1.5" style="width: 30px; height: 25px;" /> Pilots</a
+>
+<a href="/" class="text-lg text-stone-200 inline-flex" id="index"
+	><Icon icon="ri:radar-fill" class="pt-1.5" style="width: 30px; height: 25px" /> Controllers</a
+>
+<a href="/" class="text-lg text-stone-200 inline-flex" id="index"
+	><Icon icon="ic:baseline-people" class="pt-1.5" style="width: 30px; height: 25px" /> Membership</a
+>
+<a href="/" class="text-lg text-stone-200 inline-flex" id="index"
+	><Icon icon="ion:calendar" class="pt-1.5" style="width: 30px; height: 25px" /> Events</a
+>
+<a href="/" class="text-lg text-stone-200 inline-flex" id="index"
+	><Icon icon="ic:baseline-log-in" class="pt-1.5" style="width: 30px; height: 25px" /> Login</a
+>
