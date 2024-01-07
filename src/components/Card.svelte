c5aceffabf8ebf6febcdd8081ecd5dd920ad0977
Deonte Palmer <ferfrink3577@gmail.com>
2024-01-07 00:50:51 -0600

    :sparkles: Formatted files + removed m-4 for every Card placement on main page and added to component

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -1,18 +1,22 @@
 <script lang="ts">
-    import Icon from '@iconify/svelte';
-    export let title: string;
-    export let subtext: string;
-    export let timestamp: string;
-    export let icon: string;
+	import Icon from '@iconify/svelte';
+	export let title: string;
+	export let subtext: string;
+	export let timestamp: string;
+	export let icon: string;
 </script>
 
-<div class="inline-flex max-w-sm m-2 overflow-hidden rounded shadow-lg border-3 border-l-red-500 col-xl-4 col-md-6 mb-4">
-    <div class="flex-grow px-6 py-4 flex flex-col justify-between">
-        <div class="mt-1 text-lg font-light uppercase">{title}</div>
-        <p class="font-bold text-2xl text-gray-700 uppercase">{subtext}</p>
-        <div class="mb-2 text-lg font-light uppercase">{timestamp}</div>
-    </div>
-    <div class="flex-none p-4 flex items-center justify-center">
-        <Icon icon={icon} class="" width="64" height="64" />
-    </div>
+<div class="m-4">
+	<div
+		class="inline-flex max-w-sm m-2 overflow-hidden rounded shadow-lg border-3 border-l-red-500 col-xl-4 col-md-6 mb-4"
+	>
+		<div class="flex-grow px-6 py-4 flex flex-col justify-between">
+			<div class="mt-1 text-lg font-light uppercase">{title}</div>
+			<p class="font-bold text-2xl text-gray-700 uppercase">{subtext}</p>
+			<div class="mb-2 text-lg font-light uppercase">{timestamp}</div>
+		</div>
+		<div class="flex-none p-4 flex items-center justify-center">
+			<Icon {icon} class="" width="64" height="64" />
+		</div>
+	</div>
 </div>
