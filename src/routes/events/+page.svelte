97d7df8d2d57f7e5a3a7f6f7b61905233222742e
Deonte Palmer <ferfrink3577@gmail.com>
2024-01-23 13:04:13 -0600

    :sparkles: :fire: Update events page hero header

--- src/routes/events/+page.svelte
+++ src/routes/events/+page.svelte
@@ -7,7 +7,7 @@
   console.log(data);
 </script>
 
-<header class="bg-gray-700 block" id="myTopnav">
+<!--<header class="bg-gray-700 block" id="myTopnav">
   <div class="justify-between flex flex-row max-w-6xl h-16 items-center my-0 mx-auto">
     <Navbar />
   </div>
@@ -19,7 +19,26 @@
       </div>
     </div>
   </div>
+</header>-->
+
+<header class="bg-gray-700 block" id="myTopnav">
+	<div class="justify-between flex flex-row max-w-6xl h-16 items-center my-0 mx-auto">
+		<Navbar />
+	</div>
+	<div
+		style="background-position: 0% 50%; background-size: cover; background-image: url('/KJAXNIGHT.png'); left: 0; top: 0; height: 400px; "
+	>
+		<div
+			class="w-full flex flex-col justify-center items-center container text-center m-auto p-[5rem]"
+		>
+			<img src="/ZJX-Light-Logo.png" height="100" width="100" alt="" srcset="" />
+			<h1 class="text-6xl text-white font-bold pt-3">Events Center</h1>
+			<h3 class="text-3xl text-white pt-3">Jacksonville ARTCC</h3>
+		</div>
+	</div>
 </header>
+
+
 <div id="breadcrumbs" class="border-b">
   <div class="py-1.5 text-center">
     <nav class="py-2 mb-0">
