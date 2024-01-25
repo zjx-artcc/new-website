0dd91308709e873be8b368c144e3b3810fdaea40
svalencia014 <sv.samuel.valencia014@gmail.com>
2024-01-25 09:46:03 -0500

    fix conflicts

--- src/routes/events/[id]/+page.server.js
+++ src/routes/events/[id]/+page.server.js
@@ -1,19 +1,13 @@
 //@ts-nocheck
-
-import { supabase } from '../../../lib/supabaseClient';
+import { error } from '@sveltejs/kit'
+//import { supabase } from '../../../lib/supabaseClient';
 
 /** @type {import('$types').PageLoad}*/
 // eslint-disable-next-line no-unused-vars
 export async function load({ params }) {
   console.log(params);
   if (params.id == undefined) {
-    return {
-      error: {
-        status: 404,
-        message: "Event not found"
-      }
-    
-    }
+    error(404, 'Event not found');
   }
   /*const { data, error } = await supabase.from('events').select("*").eq('id', params.id).single();
   if (error) {
