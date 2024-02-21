cd9bbdd06457173774e9fa454225f0743438071a
Deonte Palmer <ferfrink3577@gmail.com>
2024-02-20 22:23:44 -0600

    :sparkles: refurbished

--- src/routes/membership/roster/[cid]/+page.svelte
+++ src/routes/membership/roster/[cid]/+page.svelte
@@ -25,10 +25,6 @@
 	</div>
 </header>
 
-<!--
-// v0 by Vercel.
-// https://v0.dev/t/VUb1m2cqJMR
--->
 <div class="flex flex-col w-full min-h-screen">
 	<main class="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-6">
 		<div class="flex flex-col gap-2 md:flex-row md:items-center md:gap-4 lg:gap-8">
