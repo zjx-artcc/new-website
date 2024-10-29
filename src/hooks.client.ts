6353cbd4da2be0ca32bb2d391105a0527357e2ed
svalencia014 <sv.samuel.valencia014@gmail.com>
2024-10-29 07:31:21 -0400

    how does this conflict

---
+++ src/hooks.client.ts
@@ -0,0 +1,12 @@
+import * as Sentry from "@sentry/sveltekit";
+
+Sentry.init({
+    dsn: "https://726cac664c16ca75bb5e53fc75ac3712@o4508141635239936.ingest.us.sentry.io/4508141636222976",
+    tracesSampleRate: 1.0
+});
+
+const myErrorHandler = ({error, event}) => {
+    console.error("A client side error occured:", error, event);
+}
+
+export const handleError = Sentry.handleErrorWithSentry(myErrorHandler);
--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,3 +1,15 @@
+import * as Sentry from "@sentry/sveltekit";
+import { authHandle } from "./auth";
+import { sequence } from "@sveltejs/kit/hooks";
 
+Sentry.init({
+    dsn: "https://726cac664c16ca75bb5e53fc75ac3712@o4508141635239936.ingest.us.sentry.io/4508141636222976",
+    tracesSampleRate: 1.0,
+});
 
-export { handle } from "./auth";
+const myErrorHandler = ({ error, event }) => {
+    console.error("A server side error occured:", error, event);
+}
+export const handleError = Sentry.handleErrorWithSentry(myErrorHandler);
+
+export const handle = sequence(Sentry.sentryHandle(), authHandle);
