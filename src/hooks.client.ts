e3362ac5b9ec95e2a422fd69617e129fe2bf8af6
svalencia014 <sv.samuel.valencia014@gmail.com>
2024-11-08 13:32:30 -0500

    add environment to sentry

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -2,7 +2,8 @@ import * as Sentry from "@sentry/sveltekit";
 
 Sentry.init({
     dsn: "https://726cac664c16ca75bb5e53fc75ac3712@o4508141635239936.ingest.us.sentry.io/4508141636222976",
-    tracesSampleRate: 1.0
+    tracesSampleRate: 1.0,
+    environment: process.env.NODE_ENV.toLowerCase()
 });
 
 const myErrorHandler = ({error, event}) => {
