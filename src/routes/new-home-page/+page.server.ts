4240e31f7246b9da04d44df8f3ab260e4778801e
Christopher Mangan <chrisjm66@gmail.com>
2024-12-03 20:16:41 -0500

    homepage stuff

---
+++ src/routes/new-home-page/+page.server.ts
@@ -0,0 +1,159 @@
+import { getHours, getRating, prisma } from '$lib/db';
+
+import type { Stats } from '@prisma/client';
+import type { PageServerLoad } from './$types';
+
+const months = ['month_three', 'month_two', 'month_two'];
+
+export const load: PageServerLoad = async ({ cookies, locals }) => {
+  //Setup page data
+  let pageData = new PageData();
+
+  //Fetch top 3 controllers for the month
+  let targetMonth = months[(new Date().getUTCMonth() + 1) % 3]; //Current month + 1 is a numerical representation of the month, Modulo 3 returns where it is within the quarter
+  const statsData = await prisma.$queryRaw<Stats[]>`SELECT * FROM stats ORDER BY ${targetMonth} DESC LIMIT 3;`;
+  
+  //Get the top 3 controllers' names
+  for (let i = 0; i < statsData.length; i++) {
+    const user = await prisma.roster.findFirst({
+      where: {
+        cid: statsData[i].cid,
+      },
+      select: {
+        first_name: true,
+        last_name: true
+      }
+    });
+
+    //Setup member object and push it
+    let memberStats: MtdStats = {
+      hours: getHours(statsData[i][targetMonth]),
+      firstName: user.first_name,
+      lastName: user.last_name
+    }
+    pageData.stats.push(memberStats);
+  }
+
+  //Fetch last 3 controllers to join the roster
+  const rosterData = await prisma.roster.findMany({
+    take: 3,
+    orderBy: {
+      created_at: 'desc',
+    },
+    select: {
+      first_name: true,
+      last_name: true,
+      rating: true,
+      created_at: true
+    }
+  });
+
+  // Sanitize and push data
+  for (let i = 0; i < rosterData.length; i++) {
+    let member: NewController = {
+      firstName: rosterData[i].first_name,
+      lastName: rosterData[i].last_name,
+      rating: getRating(rosterData[i].rating),
+      joined: rosterData[i].created_at
+    }
+
+    pageData.newControllers.push(member);
+  }
+  //Fetch next 2 events
+  const eventsData = await prisma.event.findMany({
+    take: 2,
+    orderBy: {
+      start: 'asc',
+    }
+  });
+
+  // Sanitize and push data
+  for (let i = 0; i < eventsData.length; i++) {
+    let event: Event = {
+      name: eventsData[i].name,
+      start: eventsData[i].start,
+      id: eventsData[i].id,
+      banner: eventsData[i].banner,
+      host: eventsData[i].host
+    }
+
+    pageData.events.push(event);
+  }
+
+  //Fetch all online controllers
+  const onlineData = await prisma.controllerSession.findMany({
+    orderBy: {
+      start: 'desc'
+    },
+    where: {
+      active: true
+    }
+  })
+
+  // Sanitize and push data
+  for (let i = 0; i < onlineData.length; i++) {
+    const member = await prisma.roster.findFirst({
+      where: {
+        cid: onlineData[i].cid
+      },
+      select: {
+        first_name: true,
+        last_name: true
+      }
+    });
+    let controller: OnlineController = {
+      firstName: member.first_name,
+      lastName: member.last_name,
+      callsign: onlineData[i].callsign,
+      logon: onlineData[i].start
+    }
+
+    pageData.online.push(controller);
+  }
+
+
+  return {pageData: { ...pageData }};
+}
+
+class PageData {
+  stats: MtdStats[];
+  totalHours: string;
+  newControllers: NewController[];
+  events: Event[];
+  online: OnlineController[];
+  constructor() {
+    this.stats = [];
+    this.newControllers = [];
+    this.events = [];
+    this.online = [];
+    this.totalHours = getHours(0);
+  }
+};
+
+type MtdStats = {
+  hours: string;
+  firstName: string;
+  lastName: string;
+}
+
+type NewController = {
+  firstName: string;
+  lastName: string;
+  rating: string;
+  joined: Date;
+}
+
+type Event = {
+  name: string;
+  start: Date;
+  id: number;
+  banner: string;
+  host: string;
+}
+
+type OnlineController = {
+  firstName: string;
+  lastName: string;
+  callsign: string;
+  logon: Date;
+}
---
+++ src/routes/new-home-page/+page.svelte
@@ -0,0 +1,59 @@
+<script>
+	// @ts-nocheck
+	import '$lib/app.css';
+	import Navbar from '$lib/components/Navbar.svelte';
+	import Icon from '@iconify/svelte';
+	import Card from '$lib/components/Card.svelte';
+	import EventCard from '$lib/components/EventCard.svelte';
+	import NewsCard from '$lib/components/NewsCard.svelte';
+	import ATCCard from '$lib/components/ATCCard.svelte';
+	import Footer from '$lib/components/Footer.svelte';
+	import HomepageAtcCard from '$lib/components/HomepageATCCard.svelte';
+
+	const today = new Date();
+	export let data;
+	const pageData = data.pageData;
+	const currentMonthName = today.toLocaleString('en-US', { month: 'long' });
+	
+	const availableImages = [
+		"MCO_TOUR_SMALL-5.jpg",
+		"MCO_TOUR_SMALL-7.jpg",
+		"MCO_TOUR_SMALL-8.jpg",
+		"MCO_TOUR_SMALL-28.jpg",
+		"MCO_TOUR_SMALL-46.jpg",
+		"MCO_F11_Tour_SMALL-1.jpg",
+		"MCO_F11_Tour_SMALL-6.jpg",
+		"MCO_F11_Tour_SMALL-13.jpg",
+		"MCO_F11_Tour_SMALL-14.jpg",
+		"MCO_F11_Tour_SMALL-21.jpg",
+		"MCO_F11_Tour_SMALL-22.jpg",
+		"MCO_F11_Tour_SMALL-25.jpg",
+		"MCO_F11_Tour_SMALL-30.jpg",
+	]
+	const imageUrl = "/MCO_TOUR_SMALL-5.jpg"//availableImages[Math.random() * availableImages.length]
+</script>
+
+<svelte:head>
+	<title>Jacksonville ARTCC</title>
+</svelte:head>
+
+<div class="w-full bg-[url('MCO_F11_Tour_SMALL-25.jpg')] bg-center flex flex-row justify-center items-center">
+	<div class="flex flex-row justify-center items-center">
+		<img src="/ZJX-Light-Logo.png" alt="lt" class="w-36 h-36"/>
+		<div class="flex justify-start flex-col ml-5">
+			<h1 class="font-bold text-white text-3xl">Jacksonville ARTCC</h1>
+			<h2 class="text-white text-2xl">Pride of VATUSA South</h2>
+		</div>
+	</div>
+</div>
+
+<div class="p-5">
+	<div class="flex flex-col justify-center bg-gray-200 w-72">
+		<h2 class="text-black font-semibold text-lg border-b-2 pt-1 bg-gray-300 border-gray-500 w-full text-center mb-2">Online Controllers</h2>
+		<a href="/profile/1697197" class="px-3 pb-1 flex flex-row justify-between">
+			<h4 class="font-bold text-sm text-sky-800">JAX_TWR</h4>
+			<h4 class="text-sm justify-self-end">Christopher Mangan (S1)</h4>
+		</a>
+	</div>
+</div>
+
