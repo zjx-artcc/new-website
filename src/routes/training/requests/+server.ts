1910c70e721c2888486f8ed64543a8d02004dad6
svalencia014 <sv.samuel.valencia014@gmail.com>
2024-12-11 11:18:19 -0500

    updates

--- src/routes/training/requests/+server.ts
+++ src/routes/training/requests/+server.ts
@@ -1,7 +1,7 @@
 import type { RequestHandler } from "./$types";
 
-export const POST: RequestHandler = async ( {request} ): Promise<Response> => {
+export const POST: RequestHandler = async ( {request, locals} ): Promise<Response> => {
   const { type } = await request.json();
-  console.log(type);
+  console.log(locals.session.userId);
   return new Response('Hello world!', { status: 200 });
 }
