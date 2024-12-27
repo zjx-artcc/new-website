0ab9e658e072f0e5198e4a72e65e074ef4debed4
Christopher Mangan <chrisjm66@gmail.com>
2024-12-26 23:56:17 -0500

    training note submisson vatusa

--- src/lib/vatusaApi.ts
+++ src/lib/vatusaApi.ts
@@ -1,12 +1,49 @@
+import type { TrainingSession } from "@prisma/client";
 import { formatDate, getStaffRoles, prisma } from "./db";
 
-const formatTimeString = (time: Date) => {
-  return `${time.getUTCFullYear().toString().padStart(4, "0")}-${time.getUTCMonth().toString().padStart(2, "0")}-${time.getUTCDate().toString().padStart(2, "0")}}`
+const formatTrainingSessionTimeString = (time: Date): string => {
+  return `${time.getUTCHours().toString().padStart(2, "0")}:${time.getUTCMinutes().toString().padStart(2, "0")}`
+}
+
+const formatSoloCertTimeString = (time: Date): string => {
+  return `${time.getUTCFullYear().toString().padStart(4, "0")}-${time.getUTCMonth().toString().padStart(2, "0")}-${time.getUTCDate().toString().padStart(2, "0")} ${time.getUTCHours().toString().padStart(2, "0")}:${time.getUTCMinutes().toString().padStart(2, "0")}`
+}
+
+const getDurationString = (duration: number): string => {
+  const hours = Math.floor(duration / 3600).toString().padStart(2, "0")
+  const minutes = Math.floor(duration % 3600 / 60).toString().padStart(2, "0")
+
+  return `${hours}:${minutes}`
+}
+
+export const submitTrainingNote = async(sessionData: VATUSATrainingSession) => {
+  const dateString: string = formatTrainingSessionTimeString(sessionData.sessionDate)
+  const durationString: string = getDurationString(sessionData.duration)
+  try {
+        // VATUSA API call to add solo cert
+        const vatusaReq = await fetch(
+          `https://api.vatusa.net/v2/user/${sessionData.traineeCid}/training/record?apikey=${process.env.VATUSA_KEY}`,
+          {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            body: JSON.stringify(sessionData)
+          }
+        );
+
+        console.log(`VATUSA API: POST Training Session ${sessionData.traineeCid} INS: ${sessionData.instructorCid} - STATUS ${vatusaReq.status}`)
+
+        return new Response(null, {status: vatusaReq.status})
+  } catch(error) {
+    console.error(error)
+    return new Response(error, {status: 500})
+  }
 }
 
 export const submitSoloCert = async(cid: number, position: string) => {
   const expDate: Date = new Date(Date.now() + 2592000) // 30 days after issue date
-  const dateString: string = formatTimeString(expDate)
+  const dateString: string = formatSoloCertTimeString(expDate)
   try {
         // VATUSA API call to add solo cert
         const vatusaReq = await fetch(
@@ -31,7 +68,7 @@ export const submitSoloCert = async(cid: number, position: string) => {
 
 export const deleteSoloCert = async(cid: number, position: string) => {
   const expDate: Date = new Date(Date.now() + 2592000) // 30 days after issue date
-  const dateString: string = formatTimeString(expDate)
+  const dateString: string = formatSoloCertTimeString(expDate)
   try {
         // VATUSA API call to remove solo cert
         const vatusaReq = await fetch(
@@ -141,4 +178,19 @@ export const addVisitorToVatusa = async(requestId: number, actionCid) => {
     console.error(error)
     return new Response(error, {status: 500})
   }
+}
+
+export type VATUSATrainingSession = {
+  traineeCid: number;
+  instructorCid: number;
+  sessionDate: Date;
+  position: string;
+  duration: number; // in seconds
+  movements: number;
+  score: number; // Session Score, 1-5
+  notes: string;
+  location: number; // Session Location (0 = Classroom, 1 = Live, 2 = Sweatbox)
+  ots_status: number; // 0 = Not OTS, 1 = OTS Pass, 2 = OTS Fail, 3 = OTS Recommended
+  is_cbt: boolean;
+  solo_granted: boolean;
 }
