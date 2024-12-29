9e2c2fb0ff5f20f87c66effb711674e30fb1830b
Christopher Mangan <chrisjm66@gmail.com>
2024-12-29 01:39:03 -0500

    fix tabs, schema update, begin training slots tab

--- src/routes/admin/training-admin/+page.server.ts
+++ src/routes/admin/training-admin/+page.server.ts
@@ -4,17 +4,65 @@ export const load = async({locals}) => {
   // Get Training Requests
   const trainingRequestDb = await prisma.trainingRequest.findMany({
     select: {
-      trainee_cid: true,
-
+      student_cid: true,
+      instructor_cid: true,
+      date_assigned: true,
+      date_requested: true,
+      status: true,
+      training_type: true,
+      roster_training_requests_instructor_cidToroster: {
+        select: {
+          firstName: true,
+          lastName: true
+        }
+      },
+      roster_training_requests_student_cidToroster: {
+        select: {
+          firstName: true,
+          lastName: true
+        }
+      }
+    },
+    orderBy: {
+      date_requested: 'asc'
     },
   })
 
+  let trainingRequests: TrainingRequest[] = []
+
+  // parse database pull
+  for(let i = 0; i < trainingRequestDb.length; i++) {
+    const currentRequest = trainingRequestDb[i]
+    const student = currentRequest.roster_training_requests_student_cidToroster
+    const instructor = currentRequest.roster_training_requests_instructor_cidToroster
+    trainingRequests.push({
+      studentCid: currentRequest.student_cid,
+      instructorCid: currentRequest.instructor_cid,
+      dateAssigned: currentRequest.date_assigned,
+      dateRequested: currentRequest.date_requested,
+      status: currentRequest.status != null ? currentRequest.status : "Awaiting Assignment",
+      studentName: student.firstName + " " + student.lastName,
+      instructorName: instructor != null ? instructor.firstName + " " + instructor.lastName : "None",
+      trainingType: currentRequest.training_type
+    })
+  }
+
   const data = {
-    trainingRequests: trainingRequestDb
+    trainingRequests: trainingRequests
   }
   return data
 }
 
+type TrainingRequest = {
+  studentCid: number;
+  instructorCid: number;
+  dateAssigned: Date;
+  dateRequested: Date;
+  status: string;
+  studentName: string
+  instructorName: string
+  trainingType: string
+}
 
 type RosterData = {
   name: string;
--- src/routes/admin/training-admin/+page.svelte
+++ src/routes/admin/training-admin/+page.svelte
@@ -1,40 +1,60 @@
 <script lang="ts">
 	import { TabItem, Tabs } from "flowbite-svelte";
 	export let data;
-	console.log(data)
+	
+	const formatDate = (input: Date): string => {
+		let dateTime = new Date(input);
+		let year = dateTime.getFullYear();
+		let month = (dateTime.getMonth() + 1).toString().padStart(2, "0");
+		let day = dateTime.getDate().toString().padStart(2, "0");
+
+		return `${year}-${month}-${day}`;
+	}
 </script>
 
-<div class="m-5 flex justify-center items-center flex-col gap-y-5">
+<div class="m-5 flex justify-center items-center flex-col">
 	<div>
 			<h1 class="text-xl text-sky-500 font-bold align-left">Training Admin</h1>
 	</div>
 
-	<Tabs>
-		<TabItem open>
+	<Tabs contentClass="p-2" style="underline">
+		<TabItem>
 			<span slot="title">Your Assignments</span>
 		</TabItem>
 
-		<TabItem>
+		<TabItem open>
 			<span slot="title">Training Slots</span>
 
 			<h2>S3</h2>
-			<table class="table px-2">
-					<thead class="border-2">
-						<tr class="bg-white">
+			<table class="border-spacing-2 px-2">
+					<thead class="border-4">
+						<tr>
 							<th class="px-2">Trainee</th>
 							<th class="px-2">Instructor</th>
 							<th class="px-2">Date Requested</th>
+							<th class="px-2">Date Updated</th>
+							<th class="px-2">Status</th>
 						</tr>
 					</thead>
-			<tbody>
+			<tbody class="">
 				{#each data.trainingRequests as trainingRequest}
-					<tr>
-						<td></td>
+					<tr class="border-b-2">
+						<td class="p-1">{trainingRequest.studentName}</td>
+						<td class="px-2">{trainingRequest.instructorName}</td>
+						<td class="px-2">{formatDate(trainingRequest.dateRequested)}</td>
+						<td class="px-2">{trainingRequest.dateAssigned != null ? formatDate(trainingRequest.dateAssigned) : "None"}</td>
+						<td class="px-2">{trainingRequest.status}</td>
 					</tr>
 				{/each}
 			</tbody>
-				
-			
+		</TabItem>
+
+		<TabItem>
+			<span slot="title">Student Profile</span>
+		</TabItem>
+
+		<TabItem>
+			<span slot="title">Stats</span>
 		</TabItem>
 	</Tabs>
 </div>
