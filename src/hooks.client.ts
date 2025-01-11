fba4d8137d1020154576f002fb6a1fef6fd1d77b
svalencia014 <sv.samuel.valencia014@gmail.com>
2025-01-11 13:10:24 -0500

    attempt to fix environment detection

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -1,9 +1,11 @@
 import * as Sentry from "@sentry/sveltekit";
 
+const { MODE } = import.meta.env;
+
 Sentry.init({
   dsn: "https://22ffdff6755c2e1f38dc9054b9de714e@o4508141635239936.ingest.us.sentry.io/4508141636222976",
   tracesSampleRate: 1.0,
-  environment: process.env.NODE_ENV.toLowerCase(),
+  environment: MODE,
   integrations: [
     Sentry.feedbackIntegration({
         colorScheme: "light"
