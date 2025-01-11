b365f6cea2b3824ad1995b0899604aa5ebf6b095
svalencia014 <sv.samuel.valencia014@gmail.com>
2025-01-11 13:03:27 -0500

    add crash feedback

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -1,13 +1,18 @@
 import * as Sentry from "@sentry/sveltekit";
 
 Sentry.init({
-    dsn: "https://22ffdff6755c2e1f38dc9054b9de714e@o4508141635239936.ingest.us.sentry.io/4508141636222976",
-    tracesSampleRate: 1.0,
-    environment: process.env.NODE_ENV.toLowerCase()
+  dsn: "https://22ffdff6755c2e1f38dc9054b9de714e@o4508141635239936.ingest.us.sentry.io/4508141636222976",
+  tracesSampleRate: 1.0,
+  environment: process.env.NODE_ENV.toLowerCase(),
+  integrations: [
+    Sentry.feedbackIntegration({
+        colorScheme: "light"
+    })
+  ]
 });
 
 const myErrorHandler = ({error, event}) => {
-    console.error("A client side error occured:", error, event);
+  console.error("A client side error occured:", error, event);
 }
 
 export const handleError = Sentry.handleErrorWithSentry(myErrorHandler);
