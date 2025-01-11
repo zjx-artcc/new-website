a27748b534c580e90e73ae5df0bbd7d6bee7cf52
svalencia014 <sv.samuel.valencia014@gmail.com>
2025-01-11 13:14:04 -0500

    attempt to fix sentry again

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -7,12 +7,7 @@ const { MODE } = import.meta.env;
 Sentry.init({
   dsn: "https://22ffdff6755c2e1f38dc9054b9de714e@o4508141635239936.ingest.us.sentry.io/4508141636222976",
   tracesSampleRate: 1.0,
-  environment: MODE,
-  integrations: [
-    Sentry.feedbackIntegration({
-      colorScheme: "light"
-    })
-  ]
+  environment: MODE
 });
 
 const myErrorHandler = ({ error, event }) => {
