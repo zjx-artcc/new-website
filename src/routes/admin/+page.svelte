2d4713d653f91ebde1ecbbede345885cde71d0c4
Christopher Mangan <chrisjm66@gmail.com>
2025-02-02 11:22:39 -0500

    fix admin routing

--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -1,6 +1,8 @@
 <script>
     import "$lib/app.css"
     import LinkCard from "$lib/components/LinkCard.svelte"
+    export let data
+    console.log(data)
 </script>
 
 
@@ -11,9 +13,13 @@
 
     <div class="flex flex-wrap justify-center">
         <!--<LinkCard title="Activity Monitor"  to="admin/activity-monitor" icon="mdi:clock"/>-->
-        <LinkCard title="Training Admin"  to="admin/training-admin" icon="mdi:human-male-board-poll"/>
-        <LinkCard title="Visitor Requests"  to="admin/visitor-management" icon="mdi:account-arrow-left"/>
-        <LinkCard title="Roster Management"  to="admin/roster-management" icon="mdi:account-arrow-left"/>
+        {#if data.trainingAdmin}
+            <LinkCard title="Training Admin"  to="admin/training-admin" icon="mdi:human-male-board-poll"/>
+        {/if}
+        {#if data.superAdmin}
+            <LinkCard title="Visitor Requests"  to="admin/visitor-management" icon="mdi:account-arrow-left"/>
+            <LinkCard title="Roster Management"  to="admin/roster-management" icon="mdi:account-arrow-left"/>
+        {/if}
     </div>
     
 </div>
