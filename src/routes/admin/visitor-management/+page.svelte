b8db169d4a603dd065fa1dd72c8de7e96789fddd
Christopher Mangan <chrisjm66@gmail.com>
2025-02-02 15:51:14 -0500

    fix visit requests

--- src/routes/admin/visitor-management/+page.svelte
+++ src/routes/admin/visitor-management/+page.svelte
@@ -50,6 +50,31 @@
 		}
 	};
 
+	const getRating = (rating: number) => {
+		switch(rating) {
+			case 1:
+				return "OBS"
+			case 2:
+				return "S1"
+			case 3:
+				return "S2"
+			case 4:
+				return "S3"
+			case 5:
+				return "C1"
+			case 7:
+				return "C3"
+			case 8:
+				return "I1"
+			case 10:
+				return "I3"
+			case 11:
+				return "SUP"
+			case 12:
+				return "ADM"
+		}
+	}
+
 	const approveUsers = async () => {
 		hideConfirmationScreen();
 
